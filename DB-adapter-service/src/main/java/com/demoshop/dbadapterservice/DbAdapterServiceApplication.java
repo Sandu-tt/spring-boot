38c079b87c3c5fe28c06d94fd48f26b83c4eee38
Sandu <tharakasanu80@gmail.com>
2024-06-18 01:25:11 +0530

    first commit

---
+++ DB-adapter-service/src/main/java/com/demoshop/dbadapterservice/DbAdapterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demoshop.dbadapterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbAdapterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbAdapterServiceApplication.class, args);
+    }
+
+}
---
+++ DB-adapter-service/src/test/java/com/demoshop/dbadapterservice/DbAdapterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoshop.dbadapterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbAdapterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
